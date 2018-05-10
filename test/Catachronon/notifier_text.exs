db8483e4c6588f81a99ff2f5ffe48ab3609d3cbe
Az4reus <az4reus@gmail.com>
2018-05-10 08:11:47 +0200

    Feat(tests): Basic tests.

---
+++ test/Catachronon/notifier_text.exs
@@ -0,0 +1,12 @@
+defmodule Catachronon.NotifierTest do
+  alias Catachronon.Notifier
+  use ExUnit.Case
+  doctest Notifier
+
+  test "Simple emails being created correctly" do
+    email =
+      Catachronon.send_email("This is a test header", "this is a test body", "test@malignat.us")
+
+    assert email.from == {"Catachronon", "catachronon@malignat.us"}
+  end
+end
--- test/catachronon_test.exs
+++ test/catachronon_test.exs
@@ -1,8 +1,4 @@
 defmodule CatachrononTest do
   use ExUnit.Case
   doctest Catachronon
-
-  test "greets the world" do
-    assert Catachronon.hello() == :world
-  end
 end
