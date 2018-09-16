12f74def440eaee963b655ad10cf1c5631d13a95
Mordecai Malignatus <az4reus@gmail.com>
2018-09-16 08:52:36 +0200

    Feat(Scanner): Add config for the Scanner, clean up config.exs.

--- config/config.exs
+++ config/config.exs
@@ -1,35 +1,10 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
-
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
 
 config :catachronon, Catachronon.Mailer,
   adapter: Bamboo.MailgunAdapter,
   domain: System.get_env("MAILGUN_DOMAIN_NAME"),
   api_key: System.get_env("MAILGUN_API_KEY")
 
-# You can configure your application as:
-#
-#     config :catachronon, key: :value
-#
-# and access this configuration in your application as:
-#
-#     Application.get_env(:catachronon, :key)
-#
-# You can also configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
-
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+config :catachronon, Catachronon.Scanner,
+  watched_dir: File.cwd! <> "/test_tasks/",
+  scan_interval: 60 * 1000 # Look at the dir once a minute.
